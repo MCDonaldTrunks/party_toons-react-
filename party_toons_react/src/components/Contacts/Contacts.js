ed41f3a4d504561650f54bacff0af21c3390eb4b
MCDonaldTrunks <luis_p456@outlook.com>
2022-08-08 19:58:30 -0700

    initiated contacts component, used useStated to register inquiry contact form and implemented the use of json server as a temp database

--- party_toons_react/src/components/Contacts/Contacts.js
+++ party_toons_react/src/components/Contacts/Contacts.js
@@ -1,24 +1,90 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function Contacts() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
+
+  const submitHandler = (e)=> {
+    e.preventDefault();
+    const inquiry = { fullName, email, phoneNumber, message}
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(inquiry)
+    }).then(() => {
+      console.log('inquiry added successfully')
+    })
+  }
+
   return (
-    <Container>Contacts</Container>
-  )
+    <Container>
+      <LeftInfo>
+        <h2>Contacts</h2>
+        <p>
+          We welcome every new friend. Together we will bring some magic into
+          this world.
+        </p>
+      </LeftInfo>
+      <RightForm 
+        onSubmit={submitHandler}>
+        <input
+          placeholder="Enter your full name*"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        ></input>
+        <input
+          placeholder="Enter your phone number*"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        ></input>
+        <input
+          placeholder="Enter your email address*"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <textarea
+          placeholder="Type your message*"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <SubButton>
+          Submit
+        </SubButton>
+      </RightForm>
+    </Container>
+  );
 }
 
-export default Contacts
+export default Contacts;
 
 const Container = styled.div`
-    height: 791px;
-    width: 100%;
-    background-color: white;
-
-  
-`
+  display: flex;
+  height: 791px;
+  width: 100%;
+  background-color: white;
+`;
 
 const LeftInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  flex: 1;
+`;
+
+const RightForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+`;
+
+const SubButton = styled.button`
+  display: flex;
+
 `
+
+//json-server --watch data/db.json --port 8000
