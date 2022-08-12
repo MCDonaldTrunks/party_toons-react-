90f5421db08a32d2f78bfbbbd35da17017c72a4c
MCDonaldTrunks <luis_p456@outlook.com>
2022-08-12 02:17:12 -0700

    contacts component is about 80% finished

--- party_toons_react/src/components/Contacts/Contacts.js
+++ party_toons_react/src/components/Contacts/Contacts.js
@@ -7,9 +7,9 @@ function Contacts() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [message, setMessage] = useState("");
 
-  const submitHandler = (e)=> {
+  const submitHandler = (e) => {
     e.preventDefault();
-    const inquiry = { fullName, email, phoneNumber, message}
+    const inquiry = { fullName, email, phoneNumber, message }
 
     fetch('http://localhost:8000/blogs', {
       method: 'POST',
@@ -31,19 +31,22 @@ function Contacts() {
           </p>
           <InfoMains>
             <label>Adress</label>
-
+            <p>2038 E 10th St, Long Beach, CA 90813</p>
           </InfoMains>
           <InfoMains>
             <label>Hours of Bussiness</label>
+            <p>Monday - Saturday: 11 AM - 10 PM
+              Sunday: 11 AM - 5 PM</p>
 
           </InfoMains>
           <InfoMains>
             <label>Contacts</label>
-
+            <p>+1 (234) 567 89 00
+              mywebsite@a.weblium.com</p>
           </InfoMains>
         </LeftInfo>
         <RightBackground>
-          <RightForm 
+          <RightForm
             onSubmit={submitHandler}>
             <input
               placeholder="Enter your full name*"
@@ -88,7 +91,7 @@ const Container = styled.div`
 
 const SubContainer = styled.div`
   display: flex;
-  width: 64%;
+  width: 84%;
   height: 74%;
 `
 
@@ -97,11 +100,35 @@ const LeftInfo = styled.div`
   flex-direction: column;
   align-items: start;
   flex: 1;
-  justify-content: space-around;
+  padding: 0 15px;
+
+  p {
+    20px;
+  }
+
+  * {
+    margin-top: 25px;
+  }
+
+  h2 {
+    font-size: 32px;
+    font-weight: 400;
+    margin-top: 0;
+  }
 `;
 
 const InfoMains = styled.div`
   display: flex;
+  
+  label {
+    width: 159px;
+    font-size: 24px;
+    margin-right: 30px
+  }
+
+  p {
+    font-size: 16px;
+  }
 `
 
 const RightBackground = styled.div`
@@ -109,6 +136,8 @@ const RightBackground = styled.div`
   flex: 1;
   padding: 5rem;
   background-color: #e9e9e9;
+  padding: 0 15px;
+  background-clip: content-box;
 `
 
 const RightForm = styled.form`
@@ -116,7 +145,6 @@ const RightForm = styled.form`
   flex-direction: column;
   align-items: center;
   flex: 1;
-  justify-content: space-between;
 
   input {
     border-radius: 5px;
@@ -125,6 +153,7 @@ const RightForm = styled.form`
     background-color: rgba(67, 67, 67, 0.12);
     border-color: #d9d9d9;
     border: none;
+    margin-top: 24px;
   }
 
   textarea {
@@ -133,6 +162,8 @@ const RightForm = styled.form`
     height: 40%;
     padding: 12px 24px;
     width: 70%;
+    margin-top: 24px;
+    resize: none;
   }
 
   button:hover, button:focus {
@@ -142,13 +173,15 @@ const RightForm = styled.form`
 
 const SubButton = styled.button`
   display: flex;
-
+  width: 70%;
+  padding: 12px 24px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 56px;
+  background-color: #383838;
+  color: white;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
 `
-
-
-
-
-
 //json-server --watch data/db.json --port 8000
-
-
